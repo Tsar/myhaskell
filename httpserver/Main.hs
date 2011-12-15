584f29dc2acf5f4ddd4ce469f7e05003030a21dc
Ioann Volkov <volkov.ioann@gmail.com>
2011-12-15 20:34:06 +0300

    A bit of parsing

--- httpserver/Main.hs
+++ httpserver/Main.hs
@@ -4,14 +4,21 @@ import Network (listenOn, accept, Socket, withSocketsDo, PortID(..))
 import Control.Concurrent
 import Control.Monad
 
+makeResponseForRequest :: String -> String
+makeResponseForRequest request = if (take 5 request) == "GET /" then "HTTP/1.1 200 OK\r\n\r\ntrololo" else "HTTP/1.1 404 Not Found\r\n\r\n"
+
+main :: IO ()
 main = withSocketsDo $ do
     [portStr] <- getArgs
     let port = fromIntegral (read portStr :: Int)
     sock <- listenOn (PortNumber port)
     forever $ do
-        (handle, host, portno) <- accept sock
+        (handle, _, _) <- accept sock
         forkIO $ do
             hSetBuffering handle LineBuffering
-            msg <- hGetLine handle
-            hPutStr handle msg
+            request <- hGetLine handle
+            putStrLn request   --DEBUG CODE
+            hSetBuffering handle NoBuffering
+            hPutStr handle (makeResponseForRequest request)
+            putStrLn (makeResponseForRequest request)  --DEBUG CODE
             hClose handle
