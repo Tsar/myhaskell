d10faafc4487640ec79976ed6e8c7495749e31ed
Ioann Volkov <volkov.ioann@gmail.com>
2011-12-15 18:19:51 +0300

    Something that works as a server (not by task yet)

---
+++ httpserver/Main.hs
@@ -0,0 +1,17 @@
+import System.IO
+import System.Environment (getArgs)
+import Network (listenOn, accept, Socket, withSocketsDo, PortID(..))
+import Control.Concurrent
+import Control.Monad
+
+main = withSocketsDo $ do
+    [portStr] <- getArgs
+    let port = fromIntegral (read portStr :: Int)
+    sock <- listenOn (PortNumber port)
+    forever $ do
+        (handle, host, portno) <- accept sock
+        forkIO $ do
+            hSetBuffering handle LineBuffering
+            msg <- hGetLine handle
+            putStrLn $ "The client says: " ++ msg
+            hClose handle
