ff6f496850c90c510b0c68248866be12bb35bc2e
Ioann Volkov <volkov.ioann@gmail.com>
2011-12-18 12:44:13 +0300

    Last modifications of HTTP server

--- httpserver/Main.hs
+++ httpserver/Main.hs
@@ -15,7 +15,7 @@ makeResponseForRequest request = do
                 then do
                     fileHandle <- openFile fileName ReadMode
                     fileContents <- hGetContents fileHandle
-                    --hClose fileHandle
+                    seq fileContents (hClose fileHandle)
                     return ("HTTP/1.1 200 OK\r\nContent-Length: " ++ (show (length fileContents)) ++ "\r\n\r\n" ++ fileContents)
                 else return "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n404: Not Found"
         else return "HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\n\r\n400: Bad Request"
@@ -30,8 +30,6 @@ main = withSocketsDo $ do
         forkIO $ do
             hSetBuffering handle LineBuffering
             request <- hGetLine handle
-            putStrLn request   --DEBUG CODE
             hSetBuffering handle NoBuffering
             (makeResponseForRequest request) >>= (hPutStr handle)
-            (makeResponseForRequest request) >>= putStrLn  --DEBUG CODE
             hClose handle
