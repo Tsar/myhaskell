b241aac3b86fd9c80b76d48193f78dda02fd1446
Ioann Volkov <volkov.ioann@gmail.com>
2011-12-23 12:27:41 +0300

    Виснет на аппликации =/

--- parsenormalizer/Main.hs
+++ parsenormalizer/Main.hs
@@ -19,9 +19,11 @@ termParser = do{ char '\\'
                ; return (Abs v t)
                }
             <|>
-             do{ t1 <- termParser
-               ; t2 <- termParser
-               ; return (App t1 t2)
+             do{ char '\\'
+               ; v <- word
+               ; string " -> "
+               ; t <- termParser
+               ; return (Abs v t)
                }
             <|>
              do{ char '('
@@ -30,9 +32,15 @@ termParser = do{ char '\\'
                ; return t
                }
             <|>
+             do{ t1 <- termParser
+               ; char ' '
+               ; t2 <- termParser
+               ; return (App t1 t2)
+               }
+            <|>
              do{ v <- word
                ; return (Var v)
                }
 
 --main = parseTest termParser "\\f . (\\x . f (x x)) (\\x . f (x x))"
-main = parseTest termParser "a"
+main = parseTest termParser "x x"
