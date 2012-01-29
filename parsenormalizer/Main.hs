8552998ece071250025922937428cf515cc15067
Ioann Volkov <volkov.ioann@gmail.com>
2012-01-29 14:06:04 +0300

    Переписал всё иначе, типа по-умному

--- parsenormalizer/Main.hs
+++ parsenormalizer/Main.hs
@@ -1,41 +1,49 @@
 import Text.Parsec
 import Text.Parsec.String
+import Text.Parsec.Language (emptyDef)
+import Text.Parsec.Token
 
 import Lambda
 
-word :: Parser String
-word = many1 letter
+def :: LanguageDef st
+def = emptyDef{ identStart = letter
+              , identLetter = alphaNum
+              , reservedOpNames = ["\\", ".", "->"]
+              , reservedNames = ["let", "in"]
+              }
+
+TokenParser{ parens = m_parens
+           , identifier = m_identifier
+           , reservedOp = m_reservedOp
+           , reserved = m_reserved
+           , whiteSpace = m_whiteSpace } = makeTokenParser def
+
+applicationList t ts = foldl (\t' t'' -> App t' t'') t ts
+
+abstractionList vs t = foldr (\v' t' -> Abs v' t') t vs
+
+lambdaParser :: Parser Term
+lambdaParser = do{ reservedOp "\\"
+                 ; vs <- many1 identifier
+                 ; (reservedOp ".") <|> (reservedOp "->")
+                 ; t <- termParser
+                 ; return (abstractionList vs t)
+                 }
+              <|>
+               do{ t <- notLambdaParser
+                 ; ts <- many notLambdaParser
+                 ; return (applicationList t ts)
+                 }
+
+notLambdaParser :: Parser Term
+notLambdaParser = do{ v <- identifier
+                    ; return (Var v)
+                    }
+                 <|>
+                  parens lambdaParser
 
 termParser :: Parser Term
-termParser = do{ char '\\'
-               ; v <- word
-               ; string " . "
-               ; t <- termParser
-               ; return (Abs v t)
-               }
-            <|>
-             do{ char '\\'
-               ; v <- word
-               ; string " -> "
-               ; t <- termParser
-               ; return (Abs v t)
-               }
-            <|>
-             do{ char '('
-               ; t <- termParser
-               ; char ')'
-               ; return t
-               }
-            <|>
-             do{ t1 <- termParser
-               ; char ' '
-               ; t2 <- termParser
-               ; return (App t1 t2)
-               }
-            <|>
-             do{ v <- word
-               ; return (Var v)
-               }
+termParser = lambdaParser
 
 --main = parseTest termParser "\\f . (\\x . f (x x)) (\\x . f (x x))"
-main = parseTest termParser "x x"
+--main = parseTest termParser "x x"
